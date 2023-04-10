a3e9209044630a8d92cd56191de67ffb925acf29
Krutarth Patel <92313715+Krutarth19@users.noreply.github.com>
2023-04-11 00:26:16 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum Consecutive Ones/maximum-consecutive-ones.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int longestOnes(int n, vector<int>& nums, int k) {
+        int i=0,j=0;
+        int zeroCount=0,ans=INT_MIN;
+        
+        while(j<n){
+            if(nums[j]==0){
+                zeroCount++;
+            }
+            while(zeroCount>k){
+                if(nums[i]==0){
+                    zeroCount--;
+                }
+                i++;
+            }
+            ans=max(ans,j-i+1);
+            j++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> nums;
+        for (int i = 0; i < n; ++i) {
+            int x;
+            cin >> x;
+            nums.push_back(x);
+        }
+
+        int k;
+        cin >> k;
+
+        Solution obj;
+        cout << obj.longestOnes(n, nums, k) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
