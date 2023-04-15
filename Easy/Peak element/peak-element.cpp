bc0fd2700788b8b17b814d29e187b6925418ce8c
Krutarth Patel <92313715+Krutarth19@users.noreply.github.com>
2023-04-15 09:52:49 +0530

    Updated Solution - GfG to GitHub

--- Easy/Peak element/peak-element.cpp
+++ Easy/Peak element/peak-element.cpp
@@ -14,18 +14,27 @@ class Solution
     public:
     int peakElement(int arr[], int n)
     {
-        int low = 0;
-        int high = n-1;
-        while(low < high){
-            int mid = (low + high) >> 1;
-            if(arr[mid] > arr[mid+1]){
-                high = mid;
-            }
-            else{
-                low = mid + 1;
+        // -------------- Using Binary Search ---------------
+        // int low = 0;
+        // int high = n-1;
+        // while(low < high){
+        //     int mid = (low + high) >> 1;
+        //     if(arr[mid] > arr[mid+1]){
+        //         high = mid;
+        //     }
+        //     else{
+        //         low = mid + 1;
+        //     }
+        // }
+        // return low;
+        
+        //  ----------- Using Linear Search ------------
+        for(int i=0; i<n-1; i++){
+            if(arr[i] > arr[i+1]){
+                return i;
             }
         }
-        return low;
+        return n-1;
     }
 };
 
